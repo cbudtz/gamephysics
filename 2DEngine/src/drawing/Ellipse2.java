717586df6bb96aa24d0057470721649d7410e959
cbudtz <chrbudtz@gmail.com>
2015-08-31 09:27:25 +0200

    New ellipse

---
+++ 2DEngine/src/drawing/Ellipse2.java
@@ -0,0 +1,21 @@
+package drawing;
+
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+
+public class Ellipse2 {
+	private Point2D center;
+	private double width, height;
+	
+	public Ellipse2(Point2D center, double width, double height) {
+		super();
+		this.center = center;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Ellipse2D getCircle2D(){
+		return new Ellipse2D.Double(center.getX()-width, center.getY()-height, width*2, height*2);
+		
+	}
+}
